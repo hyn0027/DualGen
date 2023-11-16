7d98a6500ac381bda934807de92e2f5a12df4fc2
hyn0027 <hongyn20@mails.tsinghua.edu.cn>
2023-11-16 14:53:01 +0800

    rebuttal revision

--- chatGPT/request.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-python main.py --task request --API-key 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 --organization org-ub767RLfZhHrQwRu9IsYT96A --data-path C:\Users\hyn00\Desktop\hyn0027\research\chatGPT\data\input.txt  --output-path C:/Users//hyn00/Desktop/hyn0027/research/CHATGPT --target-path C:\Users\hyn00\Desktop\hyn0027\research\chatGPT\data\target.txt
-
-#!/bin/sh
-
-# python main.py --task request --API-key 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 --organization org-WOVGhnT9SqH4gDorVZKKiZ3b --data-path C:/Users/hyn00/Desktop/hyn0027/research/input.txt  --output-path C:/Users//hyn00/Desktop/hyn0027/research/CHATGPT --target-path C:/Users//hyn00/Desktop/hyn0027/research/target.txt
-
-python preprocess.py --dir-path C:\Users\hyn00\Desktop\hyn0027\research\abstract_meaning_representation_amr_2.0\data\alignments\split --output-path C:\Users\hyn00\Desktop\hyn0027\research\chatGPT\data
